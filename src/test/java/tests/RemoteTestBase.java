c4778ac0660fa05fbd500ea5b4d67de6113392f2
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-09-07 16:07:09 +0300

    redone build.gradle a little bit

--- src/test/java/tests/RemoteTestBase.java
+++ src/test/java/tests/RemoteTestBase.java
@@ -3,40 +3,30 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static helpers.AttachHelper.*;
-import static org.asynchttpclient.util.HttpConstants.Methods.OPTIONS;
 
 public class RemoteTestBase {
 
     @BeforeAll
     static void beforeAll() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
-        Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments(OPTIONS);
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        Map<String, Object> selenoidOptions = new HashMap<>();
-        selenoidOptions.put("enableVNC", true);
-        selenoidOptions.put("enableVideo", true);
-        capabilities.setCapability("selenoid:options", selenoidOptions);
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
         Configuration.browserCapabilities = capabilities;
     }
 
